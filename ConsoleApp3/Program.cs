49754af5f67d93a59e43364cba39b2448313c23a
bondarenkoigor <Bond_fh54@student.itstep.org>
2022-03-31 17:57:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("file.txt")) File.WriteAllText("file.txt", "");
+            for (int i = 0; i<10; i++)
+            {
+                Console.WriteLine($"{i}/10");
+                Console.Write("Введите имя:");
+                string name = Console.ReadLine();
+                Console.Write("Введите дату рождения(d/m/y):");
+                int day = int.Parse(Console.ReadLine()), month = int.Parse(Console.ReadLine()), year = int.Parse(Console.ReadLine());
+                File.AppendAllText("file.txt", $"{name}/{new DateTime(year, month, day).ToShortDateString()}\n");
+            }
+
+            string[] fileText;
+            if (File.Exists("file.txt")) fileText = File.ReadAllText("file.txt").Split("\n");
+            else {
+                Console.WriteLine("file not found");
+                return;
+            }
+
+            int oldestIndex = 0, youngestIndex = 0;
+            for (int i = 0; i<fileText.Length - 1; i++)
+            {
+                DateTime iDate = Convert.ToDateTime(fileText[i].Substring(fileText[i].IndexOf("/") + 1));
+                DateTime oldestDate = Convert.ToDateTime(fileText[oldestIndex].Substring(fileText[oldestIndex].IndexOf("/") + 1));
+                DateTime youngestDate = Convert.ToDateTime(fileText[youngestIndex].Substring(fileText[youngestIndex].IndexOf("/") + 1));
+                if (DateTime.Now.Subtract(iDate) > DateTime.Now.Subtract(oldestDate)) oldestIndex = i;
+                if (DateTime.Now.Subtract(iDate) < DateTime.Now.Subtract(youngestDate)) youngestIndex = i;
+            }
+            File.WriteAllText("result.txt",$"Старший: {fileText[oldestIndex]}\nМладший: {fileText[youngestIndex]}");
+        }
+    }
+}
